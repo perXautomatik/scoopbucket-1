f2795860e43beee3ff82e1981183a690405807d1
Mark Michaelis <mark@IntelliTect.com>
2020-03-29 21:45:51 -0700

    Updated with script to invoke McAfee Uninstall

--- bucket/McAfeeUninstall.ps1
+++ bucket/McAfeeUninstall.ps1
@@ -1,26 +1,14 @@
 
-Write-Host "Uninstalling McAfee Applications..."
-
-. "$PSScriptRoot\Utils.ps1"
+. $PSScriptRoot\Utils.ps1
 
 Function Uninstall-McAfeeApplications {
-    Get-Program 'McAfee*' | ForEach-Object {
+    
+    $infos = $null
+    $infos += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Where-object { $null -ne $_.DisplayName -and $_.SystemComponent -ne "1" } #| select DisplayName, Publisher, DisplayVersion, Uninstall
+    $infos | Where-Object { $_.DisplayName -like '*McAfee*' } | ForEach-Object {
         try {
-            Write-Host "Uninstalling $($_.Name)..."
-            $UninstallCmd = $_.UninstallString
-            # String up to and including the .exe
-            $UninstallExecutible = $UninstallCmd.substring(0, $UninstallCmd.IndexOf(".exe") + 4 )
-            # Any parts after the .exe		
-            $UninstallArguments = $UninstallCmd.substring($UninstallCmd.IndexOf(".exe") + 4 )
-            $parms = @{
-                "FilePath" = "$UninstallExecutible";
-                "Wait"     = $true;
-                "PassThru" = $true;
-            }
-            if (-not [string]::IsNullOrWhiteSpace($UninstallArguments)) {
-                $parms.Add("ArgumentList", "$UninstallArguments")
-            }
-            Start-Process @parms
+            $installedMsiObject.UnInstall()
         }
         catch {
             Write-Error "Error occurred: $_"
