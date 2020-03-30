6c3ee507505cb5e05923b14f95bac4fdd54b9132
Mark Michaelis <mark@IntelliTect.com>
2020-03-29 17:04:24 -0700

    Renamed OSBase to OSBasePackages

--- ToVerfiy/OSBasePackages.ps1
+++ ToVerfiy/OSBasePackages.ps1
@@ -1,13 +1,29 @@
 
-Write-Host 'Installing and configuring OSBasePackages...'
-. "$PSScriptRoot\Utils.ps1"
+Function Test-Command {
+    [CmdletBinding()]
+    param(
+        [string]$command
+    )
+    return [bool](get-command $command -ErrorAction Ignore)
+}
 
-'7zip', 'notepad2', 'Everything', 'es' 'GoogleChrome', 'SysInternals', 'WinDirStat', `
-        'fzf', 'procexp', 'powershell-core', 'ussf', 'bat', `
-        'ripgrep' | `
-    ForEach-Object { 
-        Write-Host "Installing $_..."
-        choco install -y $_
+# TODO: Consider writing as a filter.
+Function Test-ChocolateyPackageInstalled {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory)][string]$PackageName
+    )
+
+    [bool] $installed = choco list $PackageName --local-only --no-progress | Where-Object {
+        $_ -match "$PackageName\s.*"
     }
+    Write-Output $installed
+}
 
+'7zip', 'notepad2', 'Everything', 'GoogleChrome', 'SysInternals', 'WinDirStat' | Where-Object {
+    Test-ChocolateyPackageInstalled $_
+} | ForEach-Object { 
+    choco install -y $_
+}
 
