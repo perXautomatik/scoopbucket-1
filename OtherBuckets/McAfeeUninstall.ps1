d87377a430d371a8f0b8bb91982e78d37d203e28
perXautomatik <christoffer.broback@gmail.com>
2023-05-15 17:41:23 +0200

    before merge

---
+++ OtherBuckets/McAfeeUninstall.ps1
@@ -0,0 +1,30 @@
+
+Write-Host "Uninstalling McAfee Applications..."
+
+. "$PSScriptRoot\Utils.ps1"
+
+Function Uninstall-McAfeeApplications {
+    Get-Program 'McAfee*' | ForEach-Object {
+        try {
+            Write-Host "Uninstalling $($_.Name)..."
+            $UninstallCmd = $_.UninstallString
+            # String up to and including the .exe
+            $UninstallExecutible = $UninstallCmd.substring(0, $UninstallCmd.IndexOf(".exe") + 4 )
+            # Any parts after the .exe		
+            $UninstallArguments = $UninstallCmd.substring($UninstallCmd.IndexOf(".exe") + 4 )
+            $parms = @{
+                "FilePath" = "$UninstallExecutible";
+                "Wait"     = $true;
+                "PassThru" = $true;
+            }
+            if (-not [string]::IsNullOrWhiteSpace($UninstallArguments)) {
+                $parms.Add("ArgumentList", "$UninstallArguments")
+            }
+            Start-Process @parms
+        }
+        catch {
+            Write-Error "Error occurred: $_"
+        }
+    }
+}
+Uninstall-McAfeeApplications
---
+++ install.ps1
@@ -0,0 +1,50 @@
+if ((Get-ExecutionPolicy) -eq 'Restricted') {
+  Set-ExecutionPolicy Bypass -Scope Process -Force
+}
+
+#Install Chocolatey
+if (-not (Get-Command choco -ErrorAction Ignore)) {
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+
+
+# Git is required when adding an additional scoop bucket.
+if (-not (Get-Command git -ErrorAction Ignore)) {
+choco install git -y --params "/GitOnlyOnPath /NoAutoCrlf /NoShellHereIntegration"
+Set-Alias -Name git -Value "$env:ProgramFiles\Git\cmd\git.exe"
+$env:Path = "$env:Path;$env:ProgramFiles\Git\cmd\"
+} 
+
+#Install Scoop
+if (-not (Get-Command scoop -ErrorAction Ignore)) {
+  $env:SCOOP = "$env:ProgramData\scoop"
+  [environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'Machine')
+  Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+}
+else {
+function Add-ScoopBucket {
+  [CmdletBinding()]
+  param(
+    [string]$name,
+    [string]$url
+  )
+  
+  if ((scoop bucket list) -notcontains $name) {
+    Write-Host "scoop bucket add $name $url"
+    # Run in new PowerShell process to ensure git path is enabled.
+    # powershell -ExecutionPolicy unrestricted -Command scoop bucket add $name $url
+    scoop bucket add $name $url
+  }
+  else {
+    Write-Information -MessageData "Scoopbucket $name is already added."
+  }
+}
+Add-ScoopBucket -Name 'perXautomatik' -Url 'https://github.com/perXautomatik/ScoopBucket'
+
+'OSBasePackages' | ForEach-Object {
+  Write-Host "Installing $_..."
+  scoop install $_
+}
+}
