c2364715e2202418522cea50be1ad5f30d5311d2
perXautomatik <christoffer.broback@gmail.com>
2023-05-15 20:15:20 +0200

    install configurations

--- ToVerfiy/OSBasePackages.ps1
+++ ToVerfiy/OSBasePackages.ps1
@@ -2,9 +2,12 @@
 Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
-'7zip', 'notepad2', 'Everything', 'es' 'GoogleChrome', 'SysInternals', 'WinDirStat', `
-        'fzf', 'procexp', 'powershell-core', 'ussf', 'bat', `
-        'ripgrep' | `
+'notepad2', 'Everything', 'opera-gx', 'SysInternals', 'wiztree', `
+        'powershell-core','echoargs','psreadline', `
+        'folder-marker','rclone-browser', 'sagemath', 'soundswitch', 'steam-library-manager','text-grab',`
+        'beyondcompare', 'autohotkey','capture2text','ditto', `
+        'freefilesync','freemind', `
+        'hardlinkshellext','irfanview','lockhunter','nirlauncher' |
     ForEach-Object { 
         Write-Host "Installing $_..."
         choco install -y $_
