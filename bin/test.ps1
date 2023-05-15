ee9f71fe2be99a6109f92d54f2c327af93095c61
perXautomatik <christoffer.broback@gmail.com>
2023-05-15 18:43:27 +0200

    merged B:\ToGit\projectFolderBare\Archive\windowsadmin\my-windows\scoopbucket

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,15 +1,8 @@
-#Requires -Version 5.1
-#Requires -Modules @{ ModuleName = 'BuildHelpers'; ModuleVersion = '2.0.1' }
-#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.2.0' }
+<#
+.SYNOPSIS
+    Execute Pester tests in repository root directory.
+#>
 
-$pesterConfig = New-PesterConfiguration -Hashtable @{
-    Run    = @{
-        Path     = "$PSScriptRoot/.."
-        PassThru = $true
-    }
-    Output = @{
-        Verbosity = 'Detailed'
-    }
-}
-$result = Invoke-Pester -Configuration $pesterConfig
+$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
+
 exit $result.FailedCount
