9521c5d2b02f75fde180353d1e84e04f4ea8dae1
perXautomatik <christoffer.broback@gmail.com>
2023-05-16 16:17:55 +0200

    Modified Install To Include Xaiviers Scoop bootstrapp sboot

--- ToVerfiy/OSBasePackages.ps1
+++ ToVerfiy/OSBasePackages.ps1
@@ -3,11 +3,8 @@ Write-Host 'Installing and configuring OSBasePackages...'
 . "$PSScriptRoot\Utils.ps1"
 
 'notepad2', 'Everything', 'opera-gx', 'SysInternals', 'wiztree', `
-        'powershell-core','echoargs','psreadline', `
-        'folder-marker','rclone-browser', 'sagemath', 'soundswitch', 'steam-library-manager','text-grab',`
-        'beyondcompare', 'autohotkey','capture2text','ditto', `
-        'freefilesync','freemind', `
-        'hardlinkshellext','irfanview','lockhunter','nirlauncher' |
+'soundswitch', 'text-grab', 'beyondcompare', 'autohotkey', `
+'ditto','freemind', 'hardlinkshellext','irfanview' |
     ForEach-Object { 
         Write-Host "Installing $_..."
         choco install -y $_
