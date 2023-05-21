d8333073895bd5fe6cee801454e7c829c6a8fc27
perXautomatik <christoffer.broback@gmail.com>
2023-05-21 02:18:09 +0200

    - loose files

--- filesystem/JoinFilesInFolder.ps1
+++
@@ -1,8 +0,0 @@
-Set-PSDebug -Trace 2
-
-get-childItem | foreach {
- 
- $output = '' + $_.Directory + '\merge.txt'
-
-[System.IO.File]::AppendAllText($output , [System.IO.File]::ReadAllText($_.FullName))
-}
--- json/getSomething2.ps1
+++
@@ -1,8 +0,0 @@
-#to add adress column
-Function Get-Something 
-{
-         [CmdletBinding()] param([Parameter(ValueFromPipeline)][pscustomobject]$Thing)
-process{ ((Get-Content -Path $Thing | ConvertFrom-Json | select state).state | Convertfrom-Json).tabGroups | %{[pscustomobject] @{ creationDate = $_.creationDate; tabsmeta = $_.tabsmeta}} | forEach-object($_.tabmeta) {%{[pscustomobject] @{title = $_.title; creationDate = $_.createDate; url = $_.url}}}
-
-        }
- } 
