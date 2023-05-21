ee42bd73c3846ab21c86a01436759de9bca309dd
perXautomatik <christoffer.broback@gmail.com>
2023-05-21 02:18:09 +0200

    - loose files

--- filesystem/getFolder.ps1
+++
@@ -1,9 +0,0 @@
-Function Get-Folder($initialDirectory) {
-    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-    $FolderBrowserDialog.RootFolder = 'MyComputer'
-    if ($initialDirectory) { $FolderBrowserDialog.SelectedPath = $initialDirectory }
-    [void] $FolderBrowserDialog.ShowDialog()
-
-    return $FolderBrowserDialog.SelectedPath
-}
