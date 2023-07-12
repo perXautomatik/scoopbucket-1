2e4d4c5dfa18dc8158d866ca437b4fccad4e1d6e
perXautomatik <christoffer.broback@gmail.com>
2023-07-12 19:14:21 +0200

    with function extraction
    # Conflicts:
    #	Todo/New folder/ScoopExt-7zip.ps1

--- scripts/postinstall.ps1
+++ scripts/postinstall.ps1
@@ -1,21 +1,8 @@
-param($dir)
-
-# Create directory for custom PHP configuration
-$confd = "$dir\conf.d"
-
-if (!(Test-Path $confd)) {
-    (New-Item -Type directory $confd) | Out-Null
-}
-
-# (Get-Content "$dir\php.ini-development") | Where-Object { $_ -notmatch '^;' }  | ? {$_.trim() -ne '' } | Set-Content "$persist_dir\development.ini"
-$prod_ini = "$dir\php.ini-production"
-if(!(Test-Path "$dir\php.ini-production")) {
-    $prod_ini = "$dir\php.ini-dist"
-}
-
-# Enable extensions to be found in installation-relative folder (the default is to search C:/php)
-if(Test-Path $prod_ini) {
-    (Get-Content "$prod_ini") | ForEach-Object { $_ -replace '; extension_dir = "ext"', 'extension_dir = "ext"' } | Set-Content "$dir\php.ini"
-}
-
-Copy-Item -ErrorAction Ignore "$dir\php.ini-development" $persist_dir
+gsudo
+scoop install aria2 7zip git gsudo dark innounp lessmsi scoop-search --global --no-cache
+sudo scoop list
+Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
+[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', 'C:\ProgramData\scoop', 'Machine')
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+scoop update
+scoop checkup
