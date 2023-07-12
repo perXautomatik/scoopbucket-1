d0dfd8c3e1bed07a70bcbf911063832b9de04b8f
perXautomatik <christoffer.broback@gmail.com>
2023-07-12 18:54:56 +0200

    compressed with reusable functions

--- New folder/install-associations.ps1
+++ New folder/install-associations.ps1
@@ -12,7 +12,7 @@ The version of Packet Tracer to use. It should be in the format of X.Y.Z.
 The installation path of Packet Tracer. It should be a valid folder path.
 
 .OUTPUTS
-None. The Create-Packet TracerRegistryEntries function does not return any output.
+None. The Create-PacketTracerRegistryEntries function does not return any output.
 
 .EXAMPLE
 Create-PacketTracerRegistryEntries -Version 8.0.1 -Path "C:\Program Files\Cisco Packet Tracer 8.0"
@@ -33,6 +33,19 @@ function Create-PacketTracerRegistryEntries {
 	$Path
     )
 
+	function Create-Subkey ($Key) {
+	    New-Item $Key -Force | Out-Null
+	}
+
+	function Create-Item ($Key, $Name, $Value) {
+	    New-Item $Key -Force | Out-Null
+	    New-ItemProperty $Key -Name $Name -Value $Value -Force | Out-Null
+	}
+
+	function Create-SupportedTypes ($App) {
+	    New-Item "HKCU:\SOFTWARE\Classes\Applications\$App\SupportedTypes" -Force | Out-Null
+	}
+	
     # Get the major version from the version string
     $MajorVersion = $Version.Split('.')[0]
 
@@ -49,12 +62,30 @@ function Create-PacketTracerRegistryEntries {
     foreach ($FileExtension in $FileExtensions.GetEnumerator()) {
 
 	# Create a new item for the file extension and set its value to the registry key
-	New-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Key)" -Value $FileExtension.Value -Force | Out-Null
+	Create-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Key)" '' $FileExtension.Value
 
 	# Create a new item for the shell\open\command subkey and set its value to the Packet Tracer executable with the file argument
-	New-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Value)\shell\open\command" -Value "`"$Path\bin\PacketTracer.exe`" `"%1`"" -Force | Out-Null
+	Create-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Value)\shell\open\command" '' "`"$Path\bin\PacketTracer.exe`" `"%1`""
 
 	# Create a new item for the DefaultIcon subkey and set its value to the Packet Tracer icon file
-	New-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Value)\DefaultIcon" -Value "$Path\art\pkt.ico" -Force | Out-Null
+	Create-Item "HKCU:\SOFTWARE\Classes\$($FileExtension.Value)\DefaultIcon" '' "$Path\art\pkt.ico"
     }
 }
+
+# Define a function to test the script and handle errors
+function Test-Script {
+    try {
+	# Call the function with some sample parameters
+	Create-PacketTracerRegistryEntries -Version 8.0.1 -Path "C:\Program Files\Cisco Packet Tracer 8.0"
+
+	# Write a success message to the console
+	Write-Host "The script ran successfully." -ForegroundColor Green
+    }
+    catch {
+	# Write an error message to the console
+	Write-Host "The script encountered an error: $($_.Exception.Message)" -ForegroundColor Red
+    }
+}
+
+# Invoke the test function
+Test-Script
