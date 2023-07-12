464c4bdbd65681aede41d68c25d67c53e4ce0ca6
perXautomatik <christoffer.broback@gmail.com>
2023-07-12 16:43:17 +0200

    [Feature] ps everything

--- buckets/Utils.ps1
+++ buckets/Utils.ps1
@@ -1,4 +1,12 @@
+    function AddNabsorb ([string]$ref, [string]$relative) {
 
+        Git submodule add $ref $relative
+        echo "#---- commit"
+        git commit -m "as submodule $relative"
+        echo "#---- absorbgitdirs"
+        Git submodule absorbgitdirs $relative
+
+    }
 # TODO: Generalize
 $UserBucket = "PerXautomatik"
 
